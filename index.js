6a96ee22c48fc48b17c68f4534ab6da0f276a754
Shehroze Ali Malik <shehrozealim@gmail.com>
2026-04-04 18:45:34 +0530

    fixed cors

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ app.use(express.json({ limit: '5mb' }))
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://chat-app-client-six-ivory.vercel.app',
+    origin: ['https://chat-app-client-six-ivory.vercel.app', 'http://localhost:3000'],
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }));
 
 
