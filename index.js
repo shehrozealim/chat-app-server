87d0dc0128bd232ae505cba5099559689f105664
Shehroze Ali Malik <shehrozealim@gmail.com>
2026-04-04 19:24:04 +0530

    ws connection fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { createServer } from 'http'
 import cookieParser from 'cookie-parser'
-import { WebSocketServer, WebSocket } from 'ws'
+import WebSocket from 'ws'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
 config()
