8851741de38402632f018c770bf0b78bd0dce913
Shehroze Ali Malik <shehrozealim@gmail.com>
2026-04-05 13:21:22 +0530

    cors fix

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
 }));
 
 app.options(/(.*)/, cors({
